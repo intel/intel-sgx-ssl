738dbfcbe084a511c0f812d7fb492d0a3573c9a3
jinghe-INTC <58922257+jinghe-INTC@users.noreply.github.com>
2021-04-29 16:09:55 +0800

    Use powershell command (#83)
    * take Windows Powershell commands instead of 7-Zip
    
    Signed-off-by: Jing He <jing.j.he@intel.com>
    
    * update copyright year

--- Windows/build_all.cmd
+++ Windows/build_all.cmd
@@ -1,6 +1,6 @@
 @echo off
 Rem 
-Rem Copyright (C) 2011-2020 Intel Corporation. All rights reserved.
+Rem Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
 Rem 
 Rem Redistribution and use in source and binary forms, with or without
 Rem modification, are permitted provided that the following conditions
@@ -36,12 +36,7 @@ Rem
 set SGXSSL_VERSION=1.9.100.%errorlevel%
 
 REM Check if Prerequisites apps available
-echo "Validating Prerequisites (7z, perl, nasm)"
-7z > nul 2>&1
-if %errorlevel% neq 0 (
-	echo "Build failed, can't find 7z."
-	exit /b 1
-)
+echo "Validating Prerequisites (perl, nasm)"
 perl -v > nul 2>&1
 if %errorlevel% neq 0 (
 	echo "Build failed, can't find perl."
@@ -135,10 +130,10 @@ echo "Build completed\nZipping package  %date% %time%"
 
 cd package
 
-7z.exe a ..\sgxssl.%SGXSSL_VERSION%.zip .
+call powershell -Command "Compress-Archive -Path docs, include, lib  -DestinationPath ..\sgxssl.%SGXSSL_VERSION%.zip -Update"
 
 if %errorlevel% neq 0 (
-	echo "Failed command: [7z.exe a ..\sgxssl.%SGXSSL_VERSION%.zip package] %errorlevel%"
+	echo "Failed command: [Compress-Archive ...] %errorlevel%"
 	cd ..
 	exit /b 1
 )
