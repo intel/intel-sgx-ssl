79286e74c8f5412270309f7efc1ec38af8af1228
jinghe-INTC <58922257+jinghe-INTC@users.noreply.github.com>
2021-09-07 14:12:27 +0800

    Redefining engine set default rand (#98)
    * change ENGINE_set_default_RAND, make it return 1 directly;
    add programmer warnings
    
    Signed-off-by: Jing He <jing.j.he@intel.com>
    
    * change ENGINE_set_default_RAND return value to 0, alway error
    
    Signed-off-by: Jing He <jing.j.he@intel.com>
    
    * set the origal API ENGINE_set_default_RAND to dummy, with Windows' powershell commands
    
    Signed-off-by: Jing He <jing.j.he@intel.com>
    
    * fix
    
    Signed-off-by: Jing He <jing.j.he@intel.com>
    
    * update documentations for RAND related changes
    
    Signed-off-by: Jing He <jing.j.he@intel.com>

--- Linux/build_openssl.sh
+++ Linux/build_openssl.sh
@@ -138,6 +138,7 @@ cp x86_64-xlate.pl $OPENSSL_VERSION/crypto/perlasm/ || exit 1
 cd $SGXSSL_ROOT/../openssl_source/$OPENSSL_VERSION || exit 1
 perl Configure --config=sgx_config.conf sgx-linux-x86_64 --with-rand-seed=none $ADDITIONAL_CONF $SPACE_OPT $MITIGATION_FLAGS no-idea no-mdc2 no-rc5 no-rc4 no-bf no-ec2m no-camellia no-cast no-srp no-hw no-dso no-shared no-ssl3 no-md2 no-md4 no-ui-console no-stdio no-afalgeng -D_FORTIFY_SOURCE=2 -DGETPID_IS_MEANINGLESS -include$SGXSSL_ROOT/../openssl_source/bypass_to_sgxssl.h --prefix=$OPENSSL_INSTALL_DIR || exit 1
 
+sed -i 's/ENGINE_set_default_RAND/dummy_ENGINE_set_default_RAND/' crypto/engine/tb_rand.c
 make build_all_generated || exit 1
 
 if [[ "$MITIGATION_OPT" == "LOAD" ]]
