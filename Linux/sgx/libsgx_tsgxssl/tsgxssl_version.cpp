498eebc7d48a139b05be091c89c353b9bb3aa21f
jinghe-INTC <58922257+jinghe-INTC@users.noreply.github.com>
2021-10-08 16:54:46 +0800

    correct version numbers and fix typos (#101)
    * correct version numbers and fix typos
    
    Signed-off-by: Jing He <jing.j.he@intel.com>
    
    * add minor version
    
    Signed-off-by: Jing He <jing.j.he@intel.com>

--- Linux/sgx/libsgx_tsgxssl/tsgxssl_version.cpp
+++ Linux/sgx/libsgx_tsgxssl/tsgxssl_version.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2017 Intel Corporation. All rights reserved.
+ * Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,8 +29,8 @@
  *
  */
 
-// This srting should be updated before every release!!
-#define STRFILEVER "1.1.0"
+// This string should be updated before every release!!
+#define STRFILEVER "1.1.1k"
 
 #define __CONCAT(x, y) x/**/y
 
