71e84cdff060d8f4c7abf08aed5c078adc90f910
jinghe-INTC <jing.j.he@intel.com>
2022-11-18 10:44:52 +0800

    Update unit test code (#141)
    * update unit test code: keygen test of RSA and EC
    * update project settings
    * update Windows test code
    
    
    Signed-off-by: Jing He <jing.j.he@intel.com>

--- Linux/sgx/test_app/enclave/TestEnclave.cpp
+++ Linux/sgx/test_app/enclave/TestEnclave.cpp
@@ -44,7 +44,7 @@
 #include <openssl/err.h>
 #include <openssl/rand.h>
 
-#define ADD_ENTROPY_SIZE	32
+#define ADD_ENTROPY_SIZE    32
 
 /* 
  * printf: 
@@ -90,184 +90,191 @@ struct evp_pkey_st {
 
 void rsa_key_gen()
 {
-	BIGNUM *bn = BN_new();
-	if (bn == NULL) {
-		printf("BN_new failure: %ld\n", ERR_get_error());
-	    return;
-	}
-	int ret = BN_set_word(bn, RSA_F4);
-    if (!ret) {
-       	printf("BN_set_word failure\n");
-	    return;
-	}
-	
-	RSA *keypair = RSA_new();
-	if (keypair == NULL) {
-		printf("RSA_new failure: %ld\n", ERR_get_error());
-	    return;
-	}
-	ret = RSA_generate_key_ex(keypair, 4096, bn, NULL);
-	if (!ret) {
-        printf("RSA_generate_key_ex failure: %ld\n", ERR_get_error());
-	    return;
-	}
+    EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+    if (!ctx)
+    {
+        printf("EVP_PKEY_CTX_new_id: %ld\n", ERR_get_error());
+        return;
+    }
+    int ret = EVP_PKEY_keygen_init(ctx);
+    if (!ret)
+    {
+        printf("EVP_PKEY_keygen_init: %ld\n", ERR_get_error());
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    if (EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, 4096) <= 0)
+    {
+        printf("EVP_PKEY_CTX_set_rsa_keygen_bits: %ld\n", ERR_get_error());
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    EVP_PKEY* evp_pkey = NULL;
+    if (EVP_PKEY_keygen(ctx, &evp_pkey) <= 0)
+    {
+        printf("EVP_PKEY_keygen: %ld\n", ERR_get_error());
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    // public key - string
+    int len = i2d_PublicKey(evp_pkey, NULL);
+    unsigned char *buf = (unsigned char *) malloc (len + 1);
+    if (!buf)
+    {
+        printf("Failed in calling malloc()\n");
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    unsigned char *tbuf = buf;
+    i2d_PublicKey(evp_pkey, &tbuf);
 
-	EVP_PKEY *evp_pkey = EVP_PKEY_new();
-	if (evp_pkey == NULL) {
-		printf("EVP_PKEY_new failure: %ld\n", ERR_get_error());
-		return;
-	}
-	EVP_PKEY_assign_RSA(evp_pkey, keypair);
+    // print public key
+    printf ("{\"public\":\"");
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%02x", (unsigned char) buf[i]);
+    }
+    printf("\"}\n");
 
-	// public key - string
-	int len = i2d_PublicKey(evp_pkey, NULL);
-	unsigned char *buf = (unsigned char *) malloc (len + 1);
-	unsigned char *tbuf = buf;
-	i2d_PublicKey(evp_pkey, &tbuf);
+    free(buf);
 
-	// print public key
-	printf ("{\"public\":\"");
-	int i;
-	for (i = 0; i < len; i++) {
-	    printf("%02x", (unsigned char) buf[i]);
-	}
-	printf("\"}\n");
+    // private key - string
+    len = i2d_PrivateKey(evp_pkey, NULL);
+    buf = (unsigned char *) malloc (len + 1);
+    if (!buf)
+    {
+        printf("Failed in calling malloc()\n");
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    tbuf = buf;
+    i2d_PrivateKey(evp_pkey, &tbuf);
 
-	free(buf);
+    // print private key
+    printf ("{\"private\":\"");
+    for (i = 0; i < len; i++) {
+        printf("%02x", (unsigned char) buf[i]);
+    }
+    printf("\"}\n");
 
-	// private key - string
-	len = i2d_PrivateKey(evp_pkey, NULL);
-	buf = (unsigned char *) malloc (len + 1);
-	tbuf = buf;
-	i2d_PrivateKey(evp_pkey, &tbuf);
+    free(buf);
 
-	// print private key
-	printf ("{\"private\":\"");
-	for (i = 0; i < len; i++) {
-	    printf("%02x", (unsigned char) buf[i]);
-	}
-	printf("\"}\n");
-
-	free(buf);
-
-	BN_free(bn);
-
-	EVP_PKEY_free(evp_pkey);
-
-	if (evp_pkey->pkey.ptr != NULL) {
-	  RSA_free(keypair);
-	}
+    EVP_PKEY_free(evp_pkey);
 }
 
 void ec_key_gen()
 {
-	unsigned char entropy_buf[ADD_ENTROPY_SIZE] = {0};
-
-	RAND_add(entropy_buf, sizeof(entropy_buf), ADD_ENTROPY_SIZE);
-	RAND_seed(entropy_buf, sizeof(entropy_buf));
-
-	EC_KEY * ec = NULL;
-    int eccgroup;
-    eccgroup = OBJ_txt2nid("secp384r1");
-    ec = EC_KEY_new_by_curve_name(eccgroup);
-    if (ec == NULL) {
-    	printf("EC_KEY_new_by_curve_name failure: %ld\n", ERR_get_error());
-	    return;
+    EVP_PKEY_CTX * ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
+    if (!ctx)
+    {
+        printf("EVP_PKEY_CTX_new_id: %ld\n", ERR_get_error());
+        return;
     }
-    
-	EC_KEY_set_asn1_flag(ec, OPENSSL_EC_NAMED_CURVE);
+    int ret = EVP_PKEY_keygen_init(ctx);
+    if (!ret)
+    {
+        printf("EVP_PKEY_keygen_init: %ld\n", ERR_get_error());
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    if (EVP_PKEY_CTX_set_ec_paramgen_curve_nid(ctx, NID_secp384r1) <= 0)
+    {
+        printf("EVP_PKEY_CTX_set_ec_paramgen_curve_nid: %ld\n", ERR_get_error());
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    EVP_PKEY* ec_pkey = NULL;
+    if (EVP_PKEY_keygen(ctx, &ec_pkey) <= 0)
+    {
+        printf("EVP_PKEY_keygen: %ld\n", ERR_get_error());
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    // public key - string
+    int len = i2d_PublicKey(ec_pkey, NULL);
+    unsigned char *buf = (unsigned char *) malloc (len + 1);
+    if (!buf)
+    {
+        printf("Failed in calling malloc()\n");
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    unsigned char *tbuf = buf;
+    i2d_PublicKey(ec_pkey, &tbuf);
 
-	int ret = EC_KEY_generate_key(ec);
-	if (!ret) {
-        printf("EC_KEY_generate_key failure\n");
-	    return;
-	}
+    // print public key
+    printf ("{\"public\":\"");
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%02x", (unsigned char) buf[i]);
+    }
+    printf("\"}\n");
 
-	EVP_PKEY *ec_pkey = EVP_PKEY_new();
-	if (ec_pkey == NULL) {
-		printf("EVP_PKEY_new failure: %ld\n", ERR_get_error());
-		return;
-	}
-	EVP_PKEY_assign_EC_KEY(ec_pkey, ec);
+    free(buf);
 
-	// public key - string
-	int len = i2d_PublicKey(ec_pkey, NULL);
-	unsigned char *buf = (unsigned char *) malloc (len + 1);
-	unsigned char *tbuf = buf;
-	i2d_PublicKey(ec_pkey, &tbuf);
+    // private key - string
+    len = i2d_PrivateKey(ec_pkey, NULL);
+    buf = (unsigned char *) malloc (len + 1);
+    if (!buf)
+    {
+        printf("Failed in calling malloc()\n");
+        EVP_PKEY_CTX_free(ctx);
+        return;
+    }
+    tbuf = buf;
+    i2d_PrivateKey(ec_pkey, &tbuf);
 
-	// print public key
-	printf ("{\"public\":\"");
-	int i;
-	for (i = 0; i < len; i++) {
-	    printf("%02x", (unsigned char) buf[i]);
-	}
-	printf("\"}\n");
+    // print private key
+    printf ("{\"private\":\"");
+    for (i = 0; i < len; i++) {
+        printf("%02x", (unsigned char) buf[i]);
+    }
+    printf("\"}\n");
 
-	free(buf);
+    free(buf);
 
-	// private key - string
-	len = i2d_PrivateKey(ec_pkey, NULL);
-	buf = (unsigned char *) malloc (len + 1);
-	tbuf = buf;
-	i2d_PrivateKey(ec_pkey, &tbuf);
-
-	// print private key
-	printf ("{\"private\":\"");
-	for (i = 0; i < len; i++) {
-	    printf("%02x", (unsigned char) buf[i]);
-	}
-	printf("\"}\n");
-
-	free(buf);
-
-	EVP_PKEY_free(ec_pkey);
-	if (ec_pkey->pkey.ptr != NULL) {
-	  EC_KEY_free(ec);
-	}
+    EVP_PKEY_free(ec_pkey);
 }
 
 
 int vprintf_cb(Stream_t stream, const char * fmt, va_list arg)
 {
-	char buf[BUFSIZ] = {'\0'};
+    char buf[BUFSIZ] = {'\0'};
 
-	int res = vsnprintf(buf, BUFSIZ, fmt, arg);
-	if (res >=0) {
-		sgx_status_t sgx_ret = uprint((const char *) buf);
-		TEST_CHECK(sgx_ret);
-	}
-	return res;
+    int res = vsnprintf(buf, BUFSIZ, fmt, arg);
+    if (res >=0) {
+        sgx_status_t sgx_ret = uprint((const char *) buf);
+        TEST_CHECK(sgx_ret);
+    }
+    return res;
 }
 
-/*
 extern "C" int CRYPTO_set_mem_functions(
         void *(*m)(size_t, const char *, int),
         void *(*r)(void *, size_t, const char *, int),
         void (*f)(void *, const char *, int));
 void* priv_malloc(size_t size, const char *file, int line)
 {
-	void* addr = malloc(size);
-	
-	printf("[malloc:%s:%d] size: %d, addr: %p\n", file, line, size, addr);
-	
-	return addr;
+    void* addr = malloc(size);
+
+    printf("[malloc:%s:%d] size: %d, addr: %p\n", file, line, size, addr);
+ 
+    return addr;
 }
 void* priv_realloc(void* old_addr, size_t new_size, const char *file, int line)
 {
-	void* new_addr = realloc(old_addr, new_size);
-	
-	printf("[realloc:%s:%d] old_addr: %p, new_size: %d, new_addr: %p\n", file, line, old_addr, new_size, new_addr);
-	
-	return new_addr;
+    void* new_addr = realloc(old_addr, new_size);
+
+    printf("[realloc:%s:%d] old_addr: %p, new_size: %d, new_addr: %p\n", file, line, old_addr, new_size, new_addr);
+
+    return new_addr;
 }
 void priv_free(void* addr, const char *file, int line)
 {
-	printf("[free:%s:%d] addr: %p\n", file, line, addr);
-	
-	free(addr);
+    printf("[free:%s:%d] addr: %p\n", file, line, addr);
+
+    free(addr);
 }
-*/
 
 
 void t_sgxssl_call_apis()
@@ -287,80 +294,80 @@ void t_sgxssl_call_apis()
     printf("test rsa_key_gen completed\n");
            
     ec_key_gen();
-	printf("test ec_key_gen completed\n");
-	
+    printf("test ec_key_gen completed\n");
+
     ret = rsa_test();
     if (ret != 0)
     {
-    	printf("test rsa_test returned error %d\n", ret);
-    	exit(ret);
+        printf("test rsa_test returned error %d\n", ret);
+        exit(ret);
     }
-	printf("test rsa_test completed\n");
+    printf("test rsa_test completed\n");
 
-	ret = ec_test();
-	if (ret != 0)
+    ret = ec_test();
+    if (ret != 0)
     {
-    	printf("test ec_test returned error %d\n", ret);
-    	exit(ret);
+        printf("test ec_test returned error %d\n", ret);
+        exit(ret);
     }
-	printf("test ec_test completed\n");
-	
-	ret = ecdh_test();
-	if (ret != 0)
-    {
-    	printf("test ecdh_test returned error %d\n", ret);
-    	exit(ret);
-    }
-	printf("test ecdh_test completed\n");
-	
-	ret = ecdsa_test();
-	if (ret != 0)
-    {
-    	printf("test ecdsar_test returned error %d\n", ret);
-    	exit(ret);
-    }
-	printf("test ecdsa_test completed\n");
+    printf("test ec_test completed\n");
 
-	ret = bn_test();
-	if (ret != 0)
+    ret = ecdh_test();
+    if (ret != 0)
     {
-    	printf("test bn_test returned error %d\n", ret);
-    	exit(ret);
+        printf("test ecdh_test returned error %d\n", ret);
+        exit(ret);
     }
-	printf("test bn_test completed\n");
+    printf("test ecdh_test completed\n");
 
-	ret = dh_test();
-	if (ret != 0)
+    ret = ecdsa_test();
+    if (ret != 0)
     {
-    	printf("test dh_test returned error %d\n", ret);
-    	exit(ret);
+        printf("test ecdsar_test returned error %d\n", ret);
+        exit(ret);
     }
-	printf("test dh_test completed\n");
+    printf("test ecdsa_test completed\n");
 
-	ret = sha256_test();
-	if (ret != 0)
+    ret = bn_test();
+    if (ret != 0)
     {
-    	printf("test sha256_test returned error %d\n", ret);
-    	exit(ret);
+        printf("test bn_test returned error %d\n", ret);
+        exit(ret);
     }
-	printf("test sha256_test completed\n");
-	
-	ret = sha1_test();
-	if (ret != 0)
+    printf("test bn_test completed\n");
+
+    ret = dh_test();
+    if (ret != 0)
     {
-    	printf("test sha1_test returned error %d\n", ret);
-    	exit(ret);
+        printf("test dh_test returned error %d\n", ret);
+        exit(ret);
     }
-	printf("test sha1_test completed\n");
-	
-	ret = threads_test();
-	if (ret != 0)
+    printf("test dh_test completed\n");
+
+    ret = sha256_test();
+    if (ret != 0)
     {
-    	printf("test threads_test returned error %d\n", ret);
-    	exit(ret);
+        printf("test sha256_test returned error %d\n", ret);
+        exit(ret);
     }
-	printf("test threads_test completed\n");
-	
+    printf("test sha256_test completed\n");
+
+    ret = sha1_test();
+    if (ret != 0)
+    {
+        printf("test sha1_test returned error %d\n", ret);
+        exit(ret);
+    }
+    printf("test sha1_test completed\n");
+
+    ret = threads_test();
+    if (ret != 0)
+    {
+        printf("test threads_test returned error %d\n", ret);
+        exit(ret);
+    }
+    printf("test threads_test completed\n");
+
     //GM SM2 - sign and verify
     ret = ecall_sm2();
     if (ret != 0)
