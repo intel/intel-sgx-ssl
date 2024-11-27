d9f61e93b84e350ca74ec948e30b578f4ae492fa
Jing He <jing.j.he@intel.com>
2024-11-26 23:26:53 -0500

    Removed unused functionality from this file. "make all FIPS=1; make test FIPS=1" shows the OpenSSL FIP provider working inside an enclave.
    Signed-off-by: Jing He <jing.j.he@intel.com>

--- openssl_source/bss_file.c
+++ openssl_source/bss_file.c
@@ -184,26 +184,6 @@ static long file_ctrl(BIO *b, int cmd, long num, void *ptr)
     int st;
 
     switch (cmd) {
-    case BIO_C_FILE_SEEK:
-    case BIO_CTRL_RESET:
-        if (b->flags & BIO_FLAGS_UPLINK_INTERNAL)
-            ret = (long)UP_fseek(b->ptr, num, 0);
-        else
-            ret = (long)fseek(fp, num, 0);
-        break;
-    case BIO_CTRL_EOF:
-        if (b->flags & BIO_FLAGS_UPLINK_INTERNAL)
-            ret = (long)UP_feof(fp);
-        else
-            ret = (long)feof(fp);
-        break;
-    case BIO_C_FILE_TELL:
-    case BIO_CTRL_INFO:
-        if (b->flags & BIO_FLAGS_UPLINK_INTERNAL)
-            ret = UP_ftell(b->ptr);
-        else
-            ret = ftell(fp);
-        break;
     case BIO_C_SET_FILE_PTR:
         file_free(b);
         b->shutdown = (int)num & BIO_CLOSE;
@@ -264,79 +244,6 @@ static long file_ctrl(BIO *b, int cmd, long num, void *ptr)
 # endif
         }
         break;
-    case BIO_C_SET_FILENAME:
-        file_free(b);
-        b->shutdown = (int)num & BIO_CLOSE;
-        if (num & BIO_FP_APPEND) {
-            if (num & BIO_FP_READ)
-                OPENSSL_strlcpy(p, "a+", sizeof(p));
-            else
-                OPENSSL_strlcpy(p, "a", sizeof(p));
-        } else if ((num & BIO_FP_READ) && (num & BIO_FP_WRITE))
-            OPENSSL_strlcpy(p, "r+", sizeof(p));
-        else if (num & BIO_FP_WRITE)
-            OPENSSL_strlcpy(p, "w", sizeof(p));
-        else if (num & BIO_FP_READ)
-            OPENSSL_strlcpy(p, "r", sizeof(p));
-        else {
-            ERR_raise(ERR_LIB_BIO, BIO_R_BAD_FOPEN_MODE);
-            ret = 0;
-            break;
-        }
-# if defined(OPENSSL_SYS_MSDOS) || defined(OPENSSL_SYS_WINDOWS)
-        if (!(num & BIO_FP_TEXT))
-            OPENSSL_strlcat(p, "b", sizeof(p));
-        else
-            OPENSSL_strlcat(p, "t", sizeof(p));
-# elif defined(OPENSSL_SYS_WIN32_CYGWIN)
-        if (!(num & BIO_FP_TEXT))
-            OPENSSL_strlcat(p, "b", sizeof(p));
-# endif
-        fp = openssl_fopen(ptr, p);
-        if (fp == NULL) {
-            ERR_raise_data(ERR_LIB_SYS, get_last_sys_error(),
-                           "calling fopen(%s, %s)",
-                           ptr, p);
-            ERR_raise(ERR_LIB_BIO, ERR_R_SYS_LIB);
-            ret = 0;
-            break;
-        }
-        b->ptr = fp;
-        b->init = 1;
-        /* we did fopen -> we disengage UPLINK */
-        BIO_clear_flags(b, BIO_FLAGS_UPLINK_INTERNAL);
-        break;
-    case BIO_C_GET_FILE_PTR:
-        /* the ptr parameter is actually a FILE ** in this case. */
-        if (ptr != NULL) {
-            fpp = (FILE **)ptr;
-            *fpp = (FILE *)b->ptr;
-        }
-        break;
-    case BIO_CTRL_GET_CLOSE:
-        ret = (long)b->shutdown;
-        break;
-    case BIO_CTRL_SET_CLOSE:
-        b->shutdown = (int)num;
-        break;
-    case BIO_CTRL_FLUSH:
-        st = b->flags & BIO_FLAGS_UPLINK_INTERNAL
-                ? UP_fflush(b->ptr) : fflush((FILE *)b->ptr);
-        if (st == EOF) {
-            ERR_raise_data(ERR_LIB_SYS, get_last_sys_error(),
-                           "calling fflush()");
-            ERR_raise(ERR_LIB_BIO, ERR_R_SYS_LIB);
-            ret = 0;
-        }
-        break;
-    case BIO_CTRL_DUP:
-        ret = 1;
-        break;
-
-    case BIO_CTRL_WPENDING:
-    case BIO_CTRL_PENDING:
-    case BIO_CTRL_PUSH:
-    case BIO_CTRL_POP:
     default:
         ret = 0;
         break;
